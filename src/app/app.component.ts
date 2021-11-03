1ab1c3d536cc122f7bc48abdab776c01ec46fef1
Amber Ramesh <amramesh@iu.edu>
2021-11-03 14:11:30 -0400

    Set up Vega visualization - Install Vega libraries - Update app component to spec Vega - Update app template to configure spec parameters - Add data sources for kidney, skin and lymph node

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import { GroupType, OrderType, Configuration, Presets } from './models/parameters.model';
+import embed, { Result, VisualizationSpec } from 'vega-embed';
+import { parse } from 'papaparse';
+import { Title } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +11,164 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'tissue-bar-graphs';
+  yAxisAttr = 'count';
+  sortBy = 'None';
+  orderType = OrderType.Ascending;
+  cellTypes: string[] = [];
+  graphData: Array<Record<string, any>> = [];
+  groupBy = GroupType.None;
+  selectedConfig: number = 0;
+  configOptions:Array<Configuration> = Presets;
+  config: Configuration = Presets[0]
+  result?: Result;
+  OrderType = OrderType
+  groupOptions?: Array<Record<string, any>>
+
+  public constructor(private titleService: Title) {
+    this.titleService.setTitle('Tissue Bar Graphs')
+  }
+
+  async getCsv(url: string) : Promise<Array<Record<string, any>>> {
+    return new Promise((resolve, reject) => {
+      parse(url, {
+        download: true,
+        delimiter: ',',
+        header: true,
+        dynamicTyping: true,
+        skipEmptyLines: true,
+        complete: (result: Record<string, any>) => {
+          resolve(result.data)
+        },
+        error: reject
+      })
+    })
+  }
+  async loadDataset() {
+    // Reinitialize canvas and all parameters
+    if (this.result) {
+      this.result.finalize()
+      this.result = undefined
+    }
+    this.config = Presets[this.selectedConfig]
+    this.graphData.splice(0, this.graphData.length)
+    this.cellTypes.splice(0, this.cellTypes.length)
+    this.sortBy = 'None'
+    this.groupBy = GroupType.None
+    const uniqueCTs = new Set<string>()
+    
+    this.groupOptions = Object.entries(this.config.groupTypes)
+      .reduce((types, [name, value]) => {
+        types.push({ name, value })
+        return types
+      }, [{
+        name: 'None',
+        value: GroupType.None
+      }])
+
+    // Create master list of all datasets
+    for (const [index, title] of this.config.datasets.entries()) {
+      const csvData = await this.getCsv(`${this.config.basePath}${title}.csv`)
+      csvData.forEach(row => {
+        row['dataset'] = title
+        row['index'] = index
+        uniqueCTs.add(row['cell_type'])
+        this.graphData.push(row)
+      })
+    }
+
+    this.cellTypes = ['None'].concat(Array.from(uniqueCTs).sort())
+
+    // Create a Vega spec and embed component
+    this.specAndEmbed()
+  }
+
+  /**
+   * Common function to respec Vega, for simplicity
+   */
+  async specAndEmbed() {
+    const spec: VisualizationSpec = {
+      $schema: 'https://vega.github.io/schema/vega-lite/v4.json',
+      data: {
+        values: this.graphData
+      },
+      width: {
+        step: 50
+      },
+      height: 650,
+      mark: {
+        type: 'bar',
+        tooltip: true
+      },
+      encoding: {
+        facet: this.groupBy === GroupType.None ? undefined : {
+          field: this.groupBy,
+          type: 'ordinal',
+          spacing: 10,
+          sort: {
+            field: 'order',
+            op: 'sum',
+            order: this.orderType
+          }
+        },
+        x: {
+          field: 'dataset',
+          title: 'Dataset',
+          sort: {
+            field: 'order',
+            op: 'sum',
+            order: this.orderType
+          },
+          type: 'nominal',
+          axis: {
+            labelAngle: -25
+          },
+        },
+        y: {
+          field: this.yAxisAttr,
+          aggregate: 'sum',
+          title: this.yAxisAttr === 'count' ? 'Cell Count' : 'Cell Proportion',
+          scale: {
+            domain: this.yAxisAttr === 'percentage' ? [0, 100] : undefined
+          }
+        },
+        color: {
+          field: 'cell_type',
+          type: 'nominal',
+          scale: {
+            domain: this.cellTypes.slice(1),
+            range: Array.from(this.config.colorPalette).reverse().slice(0, this.cellTypes.length - 1)
+          },
+          title: 'Cell Type',
+          legend: {
+            symbolLimit: 100,
+            columns: Math.ceil(this.cellTypes.length / 30)
+          }
+        }
+      },
+      title: {
+        text: 'Cell Type Count Comparison',
+        anchor: 'middle',
+        fontSize: 18
+      },
+      transform: [
+        {
+          calculate: `datum.index < ${this.config.fixed} ? 
+            ${this.orderType === OrderType.Ascending ? 0 : Number.MAX_SAFE_INTEGER } - datum.index : 
+            datum.cell_type == '${this.sortBy}' ? 
+            datum.${this.yAxisAttr} : 0`,
+          as: 'order'
+        }
+      ],
+      resolve: {
+        scale: {
+          x: 'independent'
+        }
+      }
+    }
+    this.result = await embed('#vis', spec)
+  }
+  
+  ngOnInit() {
+    this.loadDataset();
+  }
 }
---
+++ src/app/models/parameters.model.ts
@@ -0,0 +1,190 @@
+enum Source {
+  AzimuthKidney = 'Azimuth Kidney',
+  GESkin = 'GE Skin',
+  CellarLymphNode = 'Cellar Lymph Node'
+}
+
+enum OrderType {
+  Ascending = 'ascending',
+  Descending = 'descending'
+}
+
+enum GroupType {
+  None = 'none',
+  Sex = 'sex',
+  Ethnicity = 'race',
+  Age = 'age',
+  Category = 'cat',
+  Exposure = 'exp'
+}
+
+// Colors from https://medialab.github.io/iwanthue/
+const colorPaletteLarge = [
+  '#db95cd',
+  '#c8feba',
+  '#ddb1f6',
+  '#c2db90',
+  '#f8b8f6',
+  '#99d99d',
+  '#a5a6e4',
+  '#e1e294',
+  '#3bb7ed',
+  '#eda17a',
+  '#6efcfa',
+  '#ffa498',
+  '#2ecdce',
+  '#e395ad',
+  '#98ffdf',
+  '#dd99a2',
+  '#87fff1',
+  '#ffb1b5',
+  '#2ebfb2',
+  '#d3a36a',
+  '#6bb2ed',
+  '#ffe79f',
+  '#8dc5ff',
+  '#c0b46a',
+  '#d5cfff',
+  '#80e2b9',
+  '#cf9fa8',
+  '#beffd4',
+  '#cfa19a',
+  '#87f0ff',
+  '#d2a186',
+  '#42bbcc',
+  '#ffd2a3',
+  '#9ddfff',
+  '#fff5b5',
+  '#b7d4ff',
+  '#e1ffca',
+  '#ffd4e4',
+  '#6ebb96',
+  '#ffd2c1',
+  '#71b6c3',
+  '#c8a580',
+  '#c0fff8',
+  '#c8a491',
+  '#ddffe6',
+  '#bca98f',
+  '#d6e8ff',
+  '#87b78b',
+  '#fdf7ff',
+  '#a3b090',
+  '#fff8ee',
+  '#89b3ba',
+  '#f7ffeb',
+  '#aeaba5',
+  '#9fb0a0'
+]
+
+const colorPaletteSmall = [
+  "#ffe5be",
+  "#8ec6f6",
+  "#f6b89c",
+  "#92f4ff",
+  "#ffb9b6",
+  "#75d0c5",
+  "#c0bbf2",
+  "#9bbc8a",
+  "#a9a8ce",
+  "#bcfeda",
+  "#bba5b6",
+  "#9ad7b1",
+  "#e4e3ff",
+  "#b3ad82",
+  "#73b5cd",
+  "#deffe0",
+  "#c2a692",
+  "#dffaff",
+  "#87b4af",
+  "#ffe7ec"
+]
+
+interface Configuration {
+  label: string,
+  basePath: string,
+  datasets: Array<string>,
+  groupTypes: Record<string, GroupType>,
+  fixed?: number,
+  colorPalette: Array<string>
+}
+
+const Presets: Array<Configuration> = [
+  {
+    label: Source.AzimuthKidney,
+    basePath: 'data/azimuth_kidney/',
+    datasets: [
+      'ASCT+B',
+      'Azimuth_L3',
+      'K1900174_2',
+      '18-162-2-M2',
+      '18-142-3-M2',
+      '18-312-2-M2',
+      'K1900175_1_R1',
+      'K1800430_3_R3',
+      'K1900019_1_R3',
+      'K1900387_1_R1',
+      'K1900387_4_R1',
+      'K2000094_2_R1',
+      '446_B3',
+      'K1800364_1',
+      '446_B1',
+      'KRP460C-1',
+      'KRP460P-1',
+      'KRP461C-1',
+      'KRP462C-1',
+      'KRP462P-1'
+    ],
+    groupTypes: {
+      Sex: GroupType.Sex,
+      Ethnicity: GroupType.Ethnicity
+    },
+    fixed: 2,
+    colorPalette: colorPaletteLarge
+  },
+  {
+    label: Source.GESkin,
+    basePath: 'data/ge_skin/',
+    datasets: [
+      'region_12',
+      'region_1',
+      'region_2',
+      'region_3',
+      'region_4',
+      'region_5',
+      // 'region_6',
+      'region_7',
+      'region_8',
+      'region_9',
+      'region_10',
+      'region_11'
+    ],
+    groupTypes: {
+      Sex: GroupType.Sex,
+      Age: GroupType.Age,
+      Category: GroupType.Category,
+      Exposure: GroupType.Exposure
+    },
+    fixed: 1,
+    colorPalette: colorPaletteSmall
+  },
+  {
+    label: Source.CellarLymphNode,
+    basePath: 'data/cellar_lymph_node/',
+    datasets: [
+      'CODEX_Florida_19-003-lymph-node-R2',
+      // 'CODEX_Florida_20-008-lymphnode10_lefthalf',
+      'CODEX_Florida_20-008-lymphnode10_righthalf'
+    ],
+    groupTypes: {},
+    fixed: 0,
+    colorPalette: colorPaletteSmall
+  }
+]
+
+export {
+  GroupType,
+  OrderType,
+  Configuration,
+  Presets
+}
